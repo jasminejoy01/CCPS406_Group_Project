6e80ca0ac744ee60f1d02181f3a1e34c51fb64fe
Jasmine Joy <55416635+jasminejoy01@users.noreply.github.com>
2022-02-05 15:13:43 -0500

    Update movement_modified.py

--- movement_modified.py
+++ movement_modified.py
@@ -15,12 +15,8 @@ Created on Sat Feb  5 14:02:08 2022
 x=0
 y=0
 position = [x,y]
-
-class direction:
+class direction: ## output: position coordinates
     def __init__(self):
-        #self.x = 0
-        #self.y = 0
-        self.currentRoom = None
         while (True):
             value = input("Please Move:\n")
             if (value == 'n' or value == 'N'):
@@ -41,29 +37,60 @@ class direction:
     def right(self):
         global position
         global x
-        x = x+1
+        x = x-1
         position = [x,y]
         print(position)
+        return position
     
     def left(self):
         global position
         global x
-        x = x-1
+        x = x+1
         position = [x,y]
         print(position)
+        return position
     
     def up(self):
-        global position
-        global y
-        y = y+1
-        position = [x,y]
-        print(position)
-    
-    def down(self):
         global position
         global y
         y = y-1
         position = [x,y]
         print(position)
-               
-direction()
+        return position
+    
+    def down(self):
+        global position
+        global y
+        y = y+1
+        position = [x,y]
+        print(position)
+      
+class GoTo: #checks
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tr = 0
+        self.tl = 0
+        self.br = 0
+        self.bl = 0
+        self.roomWidth = 1
+        self.roomLength = 1
+        self.floor = []
+    
+    def move(self, x, y):
+        print(self.tr)
+        #return cleaningBotStorage
+        
+    def listItems():
+      #open document with room description
+      # list documents with code
+      pass
+
+
+def main():
+    """Echo the input arguments to standard output"""
+    print(direction())
+    #GoTo()
+    #pass
+
+main()    

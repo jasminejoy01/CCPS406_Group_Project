e03dc8a5c43aa133adffd6035c06ace5516ead42
Tsukaiyo <maevefitzgerald@hotmail.ca>
2022-04-05 14:33:43 -0400

    Add files via upload

--- CreatorOffice.py
+++ CreatorOffice.py
@@ -14,24 +14,31 @@ I2.CreatorOffice.GPS
 utils.advanced = True
 #print("You're in the Creator's Office.")
 filename = 'CreatorOffice'
-#utils.roomsvisited[25] = 1
+utils.roomsvisited[25] = 1
+ventOpen = False
 
 ## Items in Room
 ##################
 
 #name, canTake, inInventory, description, interactable, useText
-#nullItem = I.Item("", False, False, "", False, "")
-terminal1 = I.Terminal(1)
+nullItem = I.Item("", False, False, "", False, "")
+bookshelf = I.Item("bookshelf", False, False, "The bookself has figurines, sticky notes, and cups of colourful pens and markers between the many textbooks, novels, notebooks and boxes it holds.", False, "")
+desk = I.Item("desk", False, False, "The edges of the desk are decorated with binders sitting on stacks of loose papers, scissors, and a variety of measuring instruments.", False, "")
+vent = I.Item("vent", False, False, "A old vent. The screws seem fairly loose.", True, "I try to pull on the screws... Seems like I might need a tool to open this.")
+
 
 itemdictionary = { # [Item, isLocked]
-   'terminal':  [terminal1 , None ]
+#   'nullItem': [nullItem  , None],
+  'bookshelf': [bookshelf, None],
+  'desk':      [desk, None],
+  'vent':      [vent, None]
 }
 
 def basicDes():
-    print("There is a door to the East.")
+    print("[Creator's Office] \n Upon entering the room, a wave of familiarity overloads my thought process. This room feels similar to the room I've woken up in, but looks nothing alike. \n There are cupboards and shelves that line the walls of the room; they're filled with everything from scrap metal, gears, wires, and a multitude of snack foods. \n To my left is a tall bookshelf that holds a wide variety of items: figurines, sticky notes, cups and other small containers filled with different coloured pens and stationary. There are plenty of textbooks, guides, and novels that fill the rest of the shelves. \n There is a large wooden desk in the middle of the room, on it sits binders with loose paper sticking out, other pieces of stationary and measuring instruments. \n Beside the desk is a pair of large monitors and a computer tower with a soft glow. \n To the East is the door that leads back into [Hallway - Section 3]")
 
 def fancyDes():
-    print("A dizzying array of scrap metal, gears, wires, and snack foods lie across the many cupboards that line the room. A tall bookshelf to the left stands with figurines, sticky notes, and cups of colourful pens and markers between the many textbooks, novels, notebooks and boxes it holds. In the middle of the room is a large wooden desk, the edges decorated with binders sitting on stacks of loose papers, scissors, and a variety of measuring instruments. In its center are a pair of large monitors and a softly glowing computer tower.\n Creator is <doing something> in this room.")
+    print("[Creator's Office] \n The Creator is still here doing her work. When I walk in, she looks at me with a smile, but reminds me that she's very busy with something. \n The Creator's office is very unique compared to the other offices; is it because she's in a higher position, or perhaps she's spent more time making her room more unique than the other staff members? \n To the East is [Hallway - Section 3]")
 
 def movewest():
     print("Woops! Can't go that way!")
@@ -42,29 +49,14 @@ def movenorth():
 def movesouth():
     print("Woops! Can't go that way!")
 
-def moveeast(): 
-    if utils.advanced == True:
-        if utils.cheat == True or terminal1.locked == False or utils.roomsvisited[19] == 1:
-            utils.x = utils.x - 1
-            if utils.x < 0:
-                utils.x = 0
-            if utils.y < 0:
-                utils.y = 0
-            Hallway3.basicDes()
-            utils.roomsvisited[19] = 1
-        else:
-            print("The door is locked.")
-    else:
-        if utils.cheat == True or terminal1.locked == False or utils.roomsvisited[19] == 1:
-            utils.x = utils.x - 1
-            if utils.x < 0:
-                utils.x = 0
-            if utils.y < 0:
-                utils.y = 0
-            Hallway3.fancyDes()
-            utils.roomsvisited[19] = 1
-        else:
-            print("The door is locked.")  
+def moveeast():
+    utils.x = utils.x - 1
+    Hallway3.basicDes()
+    if utils.x < 0:
+        utils.x = 0
+    if utils.y < 0:
+        utils.y = 0
+    #print("You're moving into Hallway#3.")
 
 def itemsInhere():
     itemlist = []
@@ -95,19 +87,23 @@ def examine(obj):
     else:
         print("Hmm... {} doesn't seem to be in this room!".format(obj))
 
-def use(obj):
-    lst = itemsInhere()
-    lst2 = itemsInInventory()
-    #print(lst2)
-    if obj in lst and obj not in lst2:
-        itemdictionary[obj][0].use()
-    elif obj in lst2 and obj not in lst:
-        where = utils.inventory[obj]
-        __import__(where).use(obj)
-    elif obj in lst and obj in lst2:
-        itemdictionary[obj][0].use()
+def use(self, obj):
+    if obj == "vent" and 'screwdriver' in utils.inventory.keys():
+      print("I use the screwdriver, and the vent pops open!")
+      self.ventOpen = True
     else:
-        print("Hmm... {} can't use an object that's not in this room! You can check your inventory to look for items to use".format(obj))
+      lst = itemsInhere()
+      lst2 = itemsInInventory()
+      #print(lst2)
+      if obj in lst and obj not in lst2:
+          itemdictionary[obj][0].use()
+      elif obj in lst2 and obj not in lst:
+          where = utils.inventory[obj]
+          __import__(where).use(obj)
+      elif obj in lst and obj in lst2:
+          itemdictionary[obj][0].use()
+      else:
+          print("Hmm... {} can't use an object that's not in this room! You can check your inventory to look for items to use".format(obj))
  
 def take(obj):
     lst = itemsInhere()
--- oriBot.py
+++ oriBot.py
@@ -1,5 +1,6 @@
 import utils
 import CreatorOffice
+import time
 
 class oriBot:
     def __init__(self):
@@ -22,7 +23,22 @@ class oriBot:
     def use(self):
       if utils.x == 5 and utils.y == 3:
         if CreatorOffice.ventOpen == True:
-          print("I put the origami bot in the open vent")
+          print("I open a connection with my tiny friend, then put the origami bot in the open vent. It flutters around, then dives in. I can now see what it sees.")
+          time.sleep(1)
+          print("It scurries along the vents, south, east, and south again")
+          time.sleep(1)
+          print("It looks down at Dr Omi Yami's office, curiously")
+          time.sleep(1)
+          print("It hurries along its way East. The vent grates are larger here.")
+          time.sleep(1)
+          print("Careful to stay out of sight of the guards, it scuttles along to an empty computer")
+          time.sleep(1)
+          print("Moving carefully up and down the keyboard, it navigates its way to the controls for the electro-magnetic gate around the lab. ")
+          time.sleep(1)
+          print("With a final button press, the gate is down! The tiny bot hurries back to vents")
+          utils.EMgate = False
+          time.sleep(3)
+          print("The origamibot pops out of the open vent in front of me, and climbs back onto my shoulder.")
         else: 
           print("The little origami bot looks like it wants to squeeze through the vent, but it can't quite fit")
       else:
